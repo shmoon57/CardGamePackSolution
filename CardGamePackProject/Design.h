8b8db31044ed2ed196466b89811060db9f73021d
ghlckd12 <ghlckd12@koreatech.ac.kr>
2024-06-11 19:30:52 +0900

    Update 'GameManager.h', 'GameManager.cpp', 'User.h', 'User.cpp' : 홀덤, 도둑잡기 합치기 / User 상속 관계 변경 Add 'Design.h', 'Design.cpp' : 렌더링 디자인 부분 추가

---
+++ CardGamePackProject/Design.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+class Design
+{
+
+public:
+	void cardDesign(string card, int x, int y);
+	void gotoxy(int x, int y);
+	void drawHorizontalLine(int x, int y, int length);
+	void drawVerticalLine(int x, int y, int length);
+	void drawSquare(int x, int y, int width, int height);
+	void printMyCard(vector<string> myCard);
+};
+
+class HoldemDesign :public Design
+{
+
+
+public:
+	void printCommunityCard(vector<string> myCard, int turnNum);
+
+};
